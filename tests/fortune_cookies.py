8669b14756ccca4e033519385b3eb66ebc157526
Martin Wendt <github@wwwendt.de>
2020-04-12 14:41:14 +0200

    Create fortune_cookies.py

---
+++ tests/fortune_cookies.py
@@ -0,0 +1,21 @@
+"""
+"""
+from .context import fabulist
+
+
+def demo_quotes():
+    fab = fabulist.Fabulist()
+
+    templates = [
+        "$(Verb:ing) is better than $(verb:ing).",
+        "$(Noun:an) a day keeps the $(noun:plural) away.",
+        "If you want to $(verb) $(adv:#positive), $(verb) $(adv:#positive)!",
+        'Confucius says: "The one who wants to $(verb) must $(verb) $(adv) the $(noun)!"',
+        'Soon a $(adj) $(noun) will $(verb) $(adv) your life!"',
+        ]
+    for q in fab.generate_quotes(templates, count=100):
+        print(q)
+
+
+if __name__ == '__main__':
+    demo_quotes()
