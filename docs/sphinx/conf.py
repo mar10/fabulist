9703d1768e92fdca9120d6d5893f01fb55f91693
Martin Wendt <mar10@wwwendt.de>
2024-09-15 16:57:07 +0200

    Update docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -250,7 +250,7 @@ html_static_path = ["_static"]
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ["../intro_slides.html", "../logo.png"]
+html_extra_path = ["../intro_slides.html", "../logo.png", "../logo_48x48.png"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
